0a03b14e0b3462d95007ecac95579724264ee11a
tanvirahmedmahin10 <tanvirahmedmahinsarker@gmail.com>
2026-09-12 23:01:29 +0600

    Fifth Commit

--- src/Components/Technologies/TechnologyAdd.tsx
+++ src/Components/Technologies/TechnologyAdd.tsx
@@ -8,14 +8,17 @@ interface tCardAdd {
 
 const TechnologyAdd = ({ added, setAdded }: tCardAdd) => {
   return (
+    
     <div className="grid grid-cols-1 gap-3 p-4 ">
+        
       {added.map((add, index) => {
         return (
           <div 
             key={index} 
             className="p-3 bg-white rounded-lg border border-gray-200 shadow-sm"
           >
-            
+            <div className="flex justify-between items-center">
+            <div>
             <div className="flex items-center space-x-3">
               <img 
                 src={add.icon} 
@@ -26,12 +29,18 @@ const TechnologyAdd = ({ added, setAdded }: tCardAdd) => {
                 {add.name}
               </span>
             </div>
+            
 
             
             <span className="text-xs font-medium text-gray-500 bg-gray-100 px-2.5 py-1 rounded-full shrink-0">
               {add.category}
             </span>
+            </div>
+            <div>sdadsasdasd</div>
+            </div>
+            
           </div>
+          
         );
       })}
     </div>
